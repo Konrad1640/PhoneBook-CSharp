590e880bbff47ab5e4365c65122a241820229f1d
Konrad1640 <94623540+Konrad1640@users.noreply.github.com>
2026-07-13 16:50:29 +0200

    Initial commit = PhoneBook C# project

---
+++ Contact.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace PhoneBook
+{
+    class Contact
+    {
+        public Contact(string name, string number)
+        {
+            Name = name;
+            Number = number;
+        } 
+        public string Name {get; set; }
+        public string Number {get; set;}
+    }
+}
